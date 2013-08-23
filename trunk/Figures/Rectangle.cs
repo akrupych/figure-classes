d532594cea71e7ac06f53344fcc92b975c4a2c2b
nick.programmer@gmail.com <nick.programmer@gmail.com@34464dcd-6e48-0ba6-4253-a7802bd955c6>
2013-08-23 18:22:26 +0000

    First

---
+++ trunk/Figures/Rectangle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Figures
+{
+    class Rectangle : Shape
+    {
+        #region Constructors
+        public Rectangle() { }
+        public Rectangle(int x1, int x2, int y1, int y2) 
+            :base(0,0,0)
+        {
+            X1 = x1;
+            X2 = x2;
+            Y1 = y1;
+            Y2 = y2;
+            Height = y1 - x1;
+            Width = y2 - x2;
+
+        }
+        public Rectangle(int width, int height) : base(0,0,0)
+        {
+            Height = height;
+            Width = width;
+        }
+        public Rectangle(int side) : base(0,0,0)
+        {
+            Width = Height = side;
+        }
+        
+        #endregion
+
+
+        #region Property
+        private int X1 { get; set; }
+        private int X2 { get; set; }
+        private int Y1 { get; set; }
+        private int Y2 { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        #endregion
+
+
+        #region Methods
+        public override void ChangeSize( int amountHeight, int amountWidth)
+        {
+            if (amountHeight > 0)
+                Height += amountHeight;
+            else
+                Height -= amountHeight;
+            if (amountWidth > 0)
+                Width += amountWidth;
+            else
+                Width -= amountWidth;
+            
+        }
+        #endregion
+    }
+}
